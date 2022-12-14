509ce8081adfa10098526c336959e5406a90fd9f
mars-86 <hrslash86@gmail.com>
2022-12-14 15:34:48 -0300

    feat: added string-manip submodule

--- main.c
+++ main.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include "files/handle.h"
+#include "lib/string-manip/string_manip.h"
 
 int main(void)
 {
     char dest[256], dest2[256];
     read_def("./definitions", dest, 256);
     printf("%s\n", dest);
-    rem_spaces(dest, dest);
-    printf("%s\n", dest);
+    str_remove_char(dest2, dest, ' ');
+    printf("%s\n", dest2);
 
     return 0;
 }
